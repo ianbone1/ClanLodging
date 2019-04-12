d5a7f60ea0a83ea7955e34e2a9a78b233790d191
Kyle Johnston <kyle9657@gmail.com>
2019-04-12 17:02:10 +0100

    Sets up HotelContainer

---
+++ src/containers/HotelContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HotelContainer extends Component {
+  render(){
+    return(
+      <h1>I am the Hotel Container</h1>
+    )
+  }
+
+}
+
+export default HotelContainer;

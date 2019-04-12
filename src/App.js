e3554a203f6f091a9c6a2bb42e4d2c4c3f1a5dc8
ianbone1 <ianbone1@googlemail.com>
2019-04-12 15:49:01 +0100

    removed FaceID for just now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import FaceID from './containers/FaceID.js'
+// import FaceID from './containers/FaceID.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
 
-      <FaceID />
+
 
     );
   }

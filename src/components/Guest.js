542a4e9d5380ec9ddb0763ea48df9bef4e83b811
Neil Watkins <neil.watkins1@sky.com>
2019-04-12 17:42:03 +0100

    guest components drafted

---
+++ src/components/Guest.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Guest extends Component {
+
+
+render(){
+
+  return (
+    <h2>I am a Guest</h2>
+  );
+}
+
+}
+export default Guest;
--- src/containers/HotelContainer.js
+++ src/containers/HotelContainer.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import GuestContainer from './GuestContainer'
 
 class HotelContainer extends Component {
   render(){
     return(
+      <div>
       <h1>I am the Hotel Container</h1>
+      <GuestContainer/>
+
+      </div>
     )
   }
 

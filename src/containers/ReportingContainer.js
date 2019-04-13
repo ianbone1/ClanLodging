aaae1625919e71268fcd2e917e16bda87bb2a1a6
Neil Watkins <neil.watkins1@sky.com>
2019-04-13 10:37:25 +0100

    resolving merge

---
+++ src/containers/ReportingContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ReportingContainer extends Component {
+  render(){
+    return(
+      <div>
+      <h1>I will be the Reporting Container</h1>
+
+      </div>
+    )
+  }
+
+}
+
+export default ReportingContainer;

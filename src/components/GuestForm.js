8fb8a4379d793984d64c60545e42f9c9123ddf24
Neil Watkins <neil.watkins1@sky.com>
2019-04-13 11:27:54 +0100

    guest form added

--- src/components/GuestForm.js
+++ src/components/GuestForm.js
@@ -6,7 +6,19 @@ class GuestForm extends Component {
 render(){
 
   return (
-    <h2>This is the Guest Form</h2>
+    <div>
+      <form >
+      <input type="text" placeholder = "FirstName"/>
+      <input type="text" placeholder = "LastName"/>
+      <input type ="text" placeholder="addressLine1"/>
+      <input type ="text" placeholder="Town"/>
+      <input type ="text" placeholder="Postcode"/>
+      <input type ="text" placeholder="Email"/>
+      <input type ="text" placeholder="Phone Number"/>
+      <button type="submit">Save</button>
+      </form>
+
+    </div>
   );
 }
 

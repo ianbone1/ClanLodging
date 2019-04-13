a685c8ce2c2a1c57f271dadbc6b65b16476c2172
Kyle Johnston <kyle9657@gmail.com>
2019-04-13 14:51:51 +0100

    Adds mapping for all rooms and guests

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,52 +1,58 @@
 import React, { Component } from 'react';
 
 class BookingForm extends Component{
-constructor(props){
-  super(props);
+  constructor(props){
+    super(props);
     this.state = {
-        checkinDate: '',
-        checkoutDate: '',
-        guestId: null,
-        partySize: '',
-        roomId: null,
-        checkedIn: false,
-        rate: '',
-        billPaid: false
+      checkinDate: '',
+      checkoutDate: '',
+      guestId: null,
+      partySize: '',
+      roomId: null,
+      checkedIn: false,
+      rate: '',
+      billPaid: false
     }
-  this.handleChange = this.handleChange.bind(this);
-  this.handleDates = this.handleDates.bind(this);
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDates = this.handleDates.bind(this);
+  }
 
-handleChange(event){
-  this.setState({[event.target.name]: event.target.value})
-}
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value})
+  }
 
-handleDates(event){
-  this.setState({[event.target.name]: event.target.value})
-}
+  handleDates(event){
+    this.setState({[event.target.name]: event.target.value})
+  }
 
   render(){
+    const rooms = this.props.rooms.map((room, index) =>{
+      return <option key={index} value={room.roomNumber}>Number: {room.roomNumber} {room.roomType} £{room.rate}</option>
+    })
+
+    const guests = this.props.guests.map((guest, index) => {
+      return <option value={guest.personId}>{guest.firstName} {guest.lastName}</option>
+    })
+
     return(
       <div>
-        <h3>I am Booking Form</h3>
-        <form name="dates" action="" >
-          <input name="checkinDate" type="date"  onChange = {this.handleDates}/>
-          <input name="checkoutDate"type="date"  onChange = {this.handleDates}/>
-          <input name = "partySize" type="number" placeholder = "Party size" onChange = {this.handleChange}/>
+      <h3>I am Booking Form</h3>
+      <form name="dates" action="" >
+      <input name="checkinDate" type="date"  onChange = {this.handleDates}/>
+      <input name="checkoutDate"type="date"  onChange = {this.handleDates}/>
+      <input name = "partySize" type="number" placeholder = "Party size" onChange = {this.handleChange}/>
 
-          <select name="guestId" onChange = {this.handleChange}>
-           <option disabled selected>Guest Name</option>
-           <option value="1">Johnny Bravo</option>
-           <option value="2">SpongeBob</option>
-          </select>
+      <select name="guestId" onChange = {this.handleChange}>
+      <option disabled selected>Guest Name</option>
+      {guests}
+      </select>
 
-          <select name="roomId" onChange = {this.handleChange}>
-           <option disabled selected>Pick Room</option>
-           <option value="1">Double</option>
-           <option value="2">Twin</option>
-          </select>
-
-        </form>
+      <select name="roomId" onChange = {this.handleChange}>
+      <option disabled selected>Pick Room</option>
+      {rooms}
+      </select>
+      <button type="submit">Create Booking</button>
+      </form>
       </div>
     )
   }

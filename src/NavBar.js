4bdd912946e7f2321a1ddc95490b6a8664596d32
ianbone1 <ianbone1@googlemail.com>
2019-04-15 15:33:35 +0100

    Some CSS added for NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+// import './NavBar.css'
 const NavBar = () =>{
   return(
-    <header>
-    <ul>
-
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-
-      <li>
-        <Link to="/bookings">Bookings</Link>
-      </li>
-
-      <li>
-        <Link to="/guests">Guests</Link>
-      </li>
-
-      <li>
-        <Link to="/reports">Reports</Link>
-      </li>
-
-    </ul>
-    </header>
+    <div className="top_nav_wrapper">
+      <nav className="topnav">
+        <a><Link to="/">Home</Link></a>
+        <a><Link to="/bookings">Bookings</Link></a>
+        <a><Link to="/guests">Guests</Link></a>
+        <a><Link to="/reports">Reports</Link></a>
+      </nav>
+    </div>
   )
 }
 

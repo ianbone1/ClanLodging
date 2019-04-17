0564cd8fa35f6c0efee2958cf93f9ab335c5a90b
Neil Watkins <neil.watkins1@sky.com>
2019-04-17 14:01:02 +0100

    staff list linked to db

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -4,7 +4,7 @@ import Staff from './Staff';
 
 const StaffList = (props) => {
 
-const staffNodes = props.staff.map((staff, index) =>{
+const staffNodes = props.staffs.map((staff, index) =>{
   return (
     <Staff key={index}
     firstName={staff.firstName}
@@ -27,30 +27,3 @@ const staffNodes = props.staff.map((staff, index) =>{
 
 }
 export default StaffList;
-
-
-// const BookingList = (props) => {
-//   // Filters and removes checked in bookings
-//
-//   const notCheckedIn = props.bookings.filter((booking) => booking.checkedin === false)
-//   const bookingsNode = notCheckedIn.map((booking, index) =>  {
-//     return (
-//       <li key = {index}>
-//         <div>
-//           <Booking booking={booking} handleDeleteBooking = {props.handleDeleteBooking} handleEditBooking = {props.handleEditBooking}/>
-//         </div>
-//       </li>
-//     )
-//   })
-//
-//
-//
-//     return(
-//       <ul>
-//         <h4>Upcoming Bookings</h4>
-//         {bookingsNode}
-//       </ul>
-//     )
-// }
-//
-// export default BookingList;

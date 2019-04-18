3ced12d287e7ecf5f9c281ee1acb886554fbdd0a
Kyle Johnston <kyle9657@gmail.com>
2019-04-18 15:23:38 +0100

    Added checkout functionality which stores the checked out bookings in a new array along with filtering them out of the list

--- src/containers/CheckInOutContainer.js
+++ src/containers/CheckInOutContainer.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import CheckedInList from '../components/CheckedInList';
 import NotCheckedInList from '../components/NotCheckedInList';
 
+
 class CheckInOutContainer extends Component{
 
   render(){
     return(
       <div>
       <h2>Check in and out </h2>
-      <CheckedInList bookings = {this.props.bookings} />
-      <NotCheckedInList bookings = {this.props.bookings} handleDeleteBooking = {this.props.handleDeleteBooking} handleCheckIn={this.props.handleCheckIn}/>
+      <CheckedInList handleCheckOut = {this.props.handleCheckOut} bookings = {this.props.bookings} />
+      <NotCheckedInList bookings = {this.props.bookings}
+      handleDeleteBooking = {this.props.handleDeleteBooking}
+      handleCheckIn={this.props.handleCheckIn}/>
       </div>
     )
   }
